849a4f0cd55606555b24e8ff866ae64510c8e5f1
Sanghun <ph38230119@gmail.com>
2025-07-08 02:00:32 +0900

    :tada: singleton pattern

---
+++ src/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton {
+
+	private final static Singleton instance = new Singleton();
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		return instance;
+	}
+}
